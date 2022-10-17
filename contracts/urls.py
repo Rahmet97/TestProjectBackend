4d3cdaaf890c5bf8d28ae6bfcefaf235bef78c68
Rahmet97 <ruslanovrahmet@gmail.com>
2022-10-17 15:42:51 +0500

    Saved service delete and u_type done

--- contracts/urls.py
+++ contracts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from contracts.views import ListAllServicesAPIView, ListGroupServicesAPIView, ServiceDetailAPIView, \
     UserDetailAPIView, TarifListAPIView, DeviceListAPIView, UserContractTarifDeviceCreateAPIView, OfferCreateAPIView, \
     OfferDetailAPIView, GetGroupAdminDataAPIView, ServiceCreateAPIView, DocumentCreateAPIView, SavedServiceAPIView, \
-    SelectedTarifDevicesAPIView, TarifAPIView
+    SelectedTarifDevicesAPIView, TarifAPIView, DeleteSavedService
 
 urlpatterns = [
     path('services', ListAllServicesAPIView.as_view(), name='ListAllServicesAPIView'),
@@ -20,5 +20,6 @@ urlpatterns = [
     path('document-create', DocumentCreateAPIView.as_view(), name='DocumentAPIView'),
     path('saved-service', SavedServiceAPIView.as_view(), name='SavedServiceAPIView'),
     path('selected-tarif-devices', SelectedTarifDevicesAPIView.as_view(), name='SelectedTarifDevicesAPIView'),
-    path('tarif-elements-devices', TarifAPIView.as_view(), name='TarifAPIView')
+    path('tarif-elements-devices', TarifAPIView.as_view(), name='TarifAPIView'),
+    path('delete-saved-service/<int:pk>', DeleteSavedService.as_view(), name='DeleteSavedService')
 ]
