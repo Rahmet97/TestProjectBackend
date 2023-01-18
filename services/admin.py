3dc704193e7ef1c48689bdec80abaf7ea46969b2
Rahmet97 <ruslanovrahmet@gmail.com>
2023-01-18 15:03:22 +0500

    Services admin panel changed

--- services/admin.py
+++ services/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DevicePublisher, DeviceUnit, Rack, Unit
+from .models import DevicePublisher, DeviceUnit, Rack, Unit, DeviceStatus
 
 
-admin.site.register((Rack, Unit, DevicePublisher, DeviceUnit))
+admin.site.register((Rack, Unit, DevicePublisher, DeviceUnit, DeviceStatus))
