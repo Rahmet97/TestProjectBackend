67969c2a69db12718fbf0c517af72744a22b04eb
user <user@linux.kali>
2023-02-23 19:45:55 +0500

    Status update in process..

--- one_c/urls.py
+++ one_c/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from one_c.views import CreateInvoiceAPIView, TestAPIView
+from one_c.views import CreateInvoiceAPIView
 
 urlpatterns = [
     path('create-invoice', CreateInvoiceAPIView.as_view(), name='CreateInvoiceAPIView'),
-    path('testing-api', TestAPIView.as_view(), name='TestAPIView'),
 ]
