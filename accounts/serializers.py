41a0ff5436d768b3586e6dbe9ef64612adede5f8
Rahmet97 <ruslanovrahmet@gmail.com>
2023-03-01 10:42:01 +0500

    Accounts serializers upgraded

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -102,7 +102,7 @@ class PinUserToGroupRoleSerializer(serializers.Serializer):
 class FizUserForOldContractSerializers(serializers.ModelSerializer):
     class Meta:
         model = FizUser
-        fields = ["first_name", "mid_name", "sur_name", "per_adr", "mob_phone_no", "email", "pport_no"]
+        fields = ["first_name", "mid_name", "sur_name", "per_adr", "mob_phone_no", "email", "pport_no", "pin"]
 
 
 class YurUserForOldContractSerializers(serializers.ModelSerializer):
