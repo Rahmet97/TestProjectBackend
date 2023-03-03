574f20b64f82b4f801afd3d4b6da0c0ce3aad721
user <user@linux.kali>
2023-03-03 10:52:28 +0500

    Billing testing.....

--- billing/urls.py
+++ billing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TariffCreateAPIView, CalculateTariffSummAPIView
+from .views import TariffCreateAPIView, CalculateTariffSummAPIView, ElementAPIView
 
 urlpatterns = [
     path('tarif-create', TariffCreateAPIView.as_view(), name='TariffCreateAPIView'),
     path('calculate', CalculateTariffSummAPIView.as_view(), name='CalculateTariffSummAPIView'),
+    path('element', ElementAPIView.as_view(), name='ElementAPIView'),
 ]
