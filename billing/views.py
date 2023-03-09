be10ada7c9ba87ee7e0f3134c536f76d0b3b933b
user <user@linux.kali>
2023-03-09 10:36:39 +0500

    Some changes

--- billing/views.py
+++ billing/views.py
@@ -1,12 +1,14 @@
+from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from accounts.models import Group
 from billing.models import BillingLog
 from billing.serializers import RequestSerializer
-from contracts.models import Tarif, Element, TarifLog
+from contracts.models import Tarif, Element, TarifLog, Service
 from contracts.serializers import TarifSerializer, ElementSerializer
 
 
@@ -39,27 +41,35 @@ class CalculateTariffSummAPIView(APIView):
 
     @swagger_auto_schema(query_serializer=RequestSerializer)
     def post(self, request):
-        rqst = RequestSerializer(request.data)
-        elements = request.data['elements']
-        fr = request.data['from']
-        amounts = []
-        total = 0
-        for i in elements:
-            element = Element.objects.get(pk=int(i["element"]))
-            quantity = int(i["quantity"])
-            amounts.append({
-                'element': int(i["element"]),
-                'quantity': quantity,
-                'cost_without_vat': float(element.cost) / 1.12,
-                'vat': '12%',
-                'amount_vat': float(element.cost) - float(element.cost) / 1.12,
-                'cost': float(element.cost)
-            })
-            total += float(element.cost)
-        data = {
-            'elements': amounts,
-            'total': total
-        }
+        group = Group.objects.get(pk=int(request.data['group']))
+        service = Service.objects.get(pk=int(request.data['service']))
+        tariff = Tarif.objects.get(pk=int(request.data['tariff']))
+        if service.name == "Co-location":
+            elements = request.data['elements']
+            for i in elements.keys():
+                if Element.objects.get(Q(group=group), Q(tarif__elements=0), Q(keyword=request.data['keyword'])):
+                    pass
+        # rqst = RequestSerializer(request.data)
+        # elements = request.data['elements']
+        # fr = request.data['from']
+        # amounts = []
+        # total = 0
+        # for i in elements:
+        #     element = Element.objects.get(pk=int(i["element"]))
+        #     quantity = int(i["quantity"])
+        #     amounts.append({
+        #         'element': int(i["element"]),
+        #         'quantity': quantity,
+        #         'cost_without_vat': float(element.cost) / 1.12,
+        #         'vat': '12%',
+        #         'amount_vat': float(element.cost) - float(element.cost) / 1.12,
+        #         'cost': float(element.cost)
+        #     })
+        #     total += float(element.cost)
+        # data = {
+        #     'elements': amounts,
+        #     'total': total
+        # }
         # billing_log = BillingLog.objects.create(
         #     user=request.user,
         #     fr=fr,
@@ -67,4 +77,4 @@ class CalculateTariffSummAPIView(APIView):
         #     response=data
         # )
         # billing_log.save()
-        return Response(data)
+        # return Response(data)
