27531bdb655c62862263ad1cda94779e455ec532
user <user@linux.kali>
2023-03-09 19:58:02 +0500

    Billing in process.

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
@@ -44,10 +46,49 @@ class ColocationTariffSummAPIView(APIView):
         service = Service.objects.get(pk=int(request.data['service']))
         tariff = Tarif.objects.get(pk=int(request.data['tariff']))
         count = int(request.data['count'])
-        device_count = request.data.get('device_count', None)
-        odf_count = request.data.get('odf_count', None)
-        electricity = request.data['electricity']
+        elements = request.data['elements']
+        device_count = elements['device_count']
+        odf_count = elements['odf_count']
+        electricity = elements['electricity']
 
+        amount = tariff.price * count
+        if device_count is not None and tariff.name == 'Unit-1':
+            electr = Element.objects.get(Q(keyword='electricity'), Q(cost=0), Q(tariff=tariff)).price * device_count
+        else:
+            electr = Element.objects.get(Q(keyword='electricity'), Q(cost=0), Q(tariff=tariff)).price * count
+        price_e = Element.objects.get(Q(keyword='electricity'), Q(tariff=tariff))
+        if electricity > electr:
+            price_electricity = math.ceil((electricity - electr) / price_e.quantity) * price_e.cost
+        else:
+            price_electricity = 0
+        price_o = Element.objects.get(Q(keyword='odf'), Q(tariff=tariff))
+        if odf_count is not None:
+            price_odf = (odf_count - 1) * price_o.cost
+        else:
+            price_odf = 0
 
-
-        return Response({})
+        amount += price_odf + price_electricity
+        data = {
+            'elements': [
+                {
+                    'element': tariff.name,
+                    'price': tariff.price,
+                    'count': count,
+                    'amount': tariff.price * count
+                },
+                {
+                    'element': 'electricity',
+                    'price': price_e,
+                    'count': electricity,
+                    'amount': price_electricity
+                },
+                {
+                    'element': 'odf',
+                    'price': price_o,
+                    'count': odf_count,
+                    'amount': price_odf
+                },
+            ],
+            'amount': amount
+        }
+        return Response(data)
