1a6b741fbd5009628ea90f88a2877935ab533aa4
Asliddin <asliddintukhtasinov5@gmail.com>
2023-03-27 02:43:54 +0500

    update main/serializer.py

--- main/models.py
+++ main/models.py
@@ -14,7 +14,17 @@ class TelegramBotChatIDs(models.Model):
         return self.chat_name
 
 
+class Application(models.Model):
+    user = models.ForeignKey(to=UserData, related_name="user_application", on_delete=models.CASCADE)
+    service = models.ForeignKey(to=Service, related_name="service_application", on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="F.I.SH/Tashkilot nomi", max_length=255)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    file = models.FileField(upload_to="application/files")
 
+    def __str__(self):
+        return self.name
     
-
