0e352f34452b0c08a3c78eea15443051a234a971
Asliddin <asliddintukhtasinov5@gmail.com>
2023-03-27 06:55:37 +0500

    update main app

--- main/serializers.py
+++ main/serializers.py
@@ -5,34 +5,33 @@ from accounts.serializers import FizUserSerializerForContractDetail, YurUserSeri
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
-    # user = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
     service = serializers.SerializerMethodField()
 
     class Meta:
         model = Application
-        # fields = ["user", "service", "name", "phone", "email", "message", "created_at", "file"]
-        fields = ["pk", "service", "name", "phone", "email", "message", "created_at", "file"]
+        fields = ["pk", "user", "service", "name", "phone", "email", "message", "created_at", "file"]
+        # fields = ["pk", "service", "name", "phone", "email", "message", "created_at", "file"]
 
 
-    # @staticmethod
-    # def get_user(obj):
-    #     user_obj = obj.user
-    #     print("user >>>> ", user_obj)
-    #     if user_obj.type == 1:  # fiz
-    #         serializer = FizUserSerializerForContractDetail(user_obj)
-    #         data = serializer.data
-    #         data['u_type'] = 'Fizik'
-    #     else:
-    #         serializer = YurUserSerializerForContractDetail(user_obj)
-    #         data = serializer.data
-    #         data['u_type'] = 'Yuridik'
-    #     return data
+    @staticmethod
+    def get_user(obj):
+        user_obj = obj.user
+        print("user >>>> ", user_obj)
+        if user_obj.type == 1:  # fiz
+            serializer = FizUserSerializerForContractDetail(user_obj)
+            data = serializer.data
+            data['u_type'] = 'Fizik'
+        else:
+            serializer = YurUserSerializerForContractDetail(user_obj)
+            data = serializer.data
+            data['u_type'] = 'Yuridik'
+        return data
     
     @staticmethod
     def get_service(obj):
         return {
             "pk": obj.service.pk,
             "name": obj.service.name
-            # "pk": obj.pk,
-            # "name": obj.name
         }
+    
