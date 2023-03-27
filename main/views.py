022ea6e7e7643bcdbd341c7f88e137ef56af1965
Asliddin <asliddintukhtasinov5@gmail.com>
2023-03-27 05:15:10 +0500

    update main/views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
-
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, response, generics
 
@@ -58,21 +56,21 @@ class ApplicationListRetrieveView(generics.GenericAPIView):
             serializer = self.serializer_class(queryset, many=True)
         return response.Response(serializer.data, status=status.HTTP_200_OK)
 
-# class ApplicationListView(ListAPIView):
-#     queryset = Application.objects.all()
-#     serializer_class = ApplicationSerializer
-#     permission_classes = [IsAuthenticatedAndPinnedToService]
+class ApplicationListView(ListAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = [IsAuthenticatedAndPinnedToService]
 
-#     def get_object(self):
-#         pinned_user = self.request.user
-#         if not Service.objects.filter(pinned_user=pinned_user).exists():
-#             responseErrorMessage(
-#             message="Siz Xizmat turiga biriktirilmagan siz", 
-#             status_code=status.HTTP_404_NOT_FOUND
-#         )
+    def get_object(self):
+        pinned_user = self.request.user
+        if not Service.objects.filter(pinned_user=pinned_user).exists():
+            responseErrorMessage(
+            message="Siz Xizmat turiga biriktirilmagan siz", 
+            status_code=status.HTTP_404_NOT_FOUND
+        )
         
-#         service_pk = Service.objects.get(pinned_user=pinned_user).pk
-#         object = super(ApplicationListView, self).get_object(self.queryset)
-#         object.queryset = self.queryset.filter(service__pk=service_pk)
-#         return object
+        service_pk = Service.objects.get(pinned_user=pinned_user).pk
+        object = super(ApplicationListView, self).get_object(self.queryset)
+        object.queryset = self.queryset.filter(service__pk=service_pk)
+        return object
         
