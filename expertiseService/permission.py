7a6eddbc162e82fb9c70e545f83a8f38cad0d473
Asliddin <asliddintukhtasinov5@gmail.com>
2023-03-31 16:35:05 +0500

    update expertiseService app

---
+++ expertiseService/permission.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+
+class IsRelatedToExpertise(permissions.BasePermission):
+    message = 'You must be the owner of this object.'
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        if True:
+            
+            return obj.client == request.user
+        return False
