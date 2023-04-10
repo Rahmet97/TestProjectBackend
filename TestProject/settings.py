da7c89305d0040077985f5719ec4400f8955b2b6
Asliddin <asliddintukhtasinov5@gmail.com>
2023-04-10 16:19:54 +0500

    update confirem api in contracts

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -116,7 +116,9 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001"
+    "http://127.0.0.1:3001",
+    "https://dc.unicon.uz",
+
 ]
 
 ROOT_URLCONF = 'TestProject.urls'
