51ba43b581598d64eda742d9189ea38ab6832522
Asliddin <asliddintukhtasinov5@gmail.com>
2023-04-11 18:30:39 +0500

    update one_c app

--- one_c/admin.py
+++ one_c/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register((Status, Invoice, Nomenclature))
+admin.site.register((Status, Invoice, Nomenclature, PayedInformation))
