a561b21c835a5448837aa7d0f395c3536a3505f3
user <user@linux.kali>
2023-04-18 13:49:19 +0500

    Git connection done..

--- services/admin.py
+++ services/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from .models import DevicePublisher, DeviceUnit, Rack, Unit, DeviceStatus, InternetProvider, ProviderContract
 
 
