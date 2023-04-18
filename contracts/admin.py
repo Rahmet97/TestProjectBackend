35b9d938f1363a7dc0d4eef71234f7c945e6e6da
user <user@linux.kali>
2023-04-18 13:34:08 +0500

    Git connection done.

--- contracts/admin.py
+++ contracts/admin.py
@@ -1,26 +1,135 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 admin.site.register((
-    Service,
     SavedService,
-    Element,
     UserContractTarifDevice,
     UserDeviceCount,
-    Device,
-    Offer,
-    Document,
-    Tarif,
     Contract,
-    ContractStatus,
-    AgreementStatus,
-    Status,
     Contracts_Participants,
-    ConnetMethod,
-    Participant,
-    ServiceParticipants,
     ExpertSummary,
     ExpertSummaryDocument,
 
     OldContractFile  # test
 ))
+
+class ServiceResource(resources.ModelResource):
+    class Meta:
+        model = Service
+
+
+@admin.register(Service)
+class ServiceAdmin(ImportExportModelAdmin):
+    resource_class = ServiceResource
+
+
+class ElementResource(resources.ModelResource):
+    class Meta:
+        model = Element
+
+
+@admin.register(Element)
+class ElementAdmin(ImportExportModelAdmin):
+    resource_class = ElementResource
+
+
+class DeviceResource(resources.ModelResource):
+    class Meta:
+        model = Device
+
+
+@admin.register(Device)
+class DeviceAdmin(ImportExportModelAdmin):
+    resource_class = DeviceResource
+
+
+class OfferResource(resources.ModelResource):
+    class Meta:
+        model = Offer
+
+
+@admin.register(Offer)
+class OfferAdmin(ImportExportModelAdmin):
+    resource_class = OfferResource
+
+
+class DocumentResource(resources.ModelResource):
+    class Meta:
+        model = Document
+
+
+@admin.register(Document)
+class DocumentAdmin(ImportExportModelAdmin):
+    resource_class = DocumentResource
+
+
+class TarifResource(resources.ModelResource):
+    class Meta:
+        model = Tarif
+
+
+@admin.register(Tarif)
+class TarifAdmin(ImportExportModelAdmin):
+    resource_class = TarifResource
+
+
+class ContractStatusResource(resources.ModelResource):
+    class Meta:
+        model = ContractStatus
+
+
+@admin.register(ContractStatus)
+class ContractStatusAdmin(ImportExportModelAdmin):
+    resource_class = ContractStatusResource
+
+
+class AgreementStatusResource(resources.ModelResource):
+    class Meta:
+        model = AgreementStatus
+
+
+@admin.register(AgreementStatus)
+class AgreementStatusAdmin(ImportExportModelAdmin):
+    resource_class = AgreementStatusResource
+
+
+class StatusResource(resources.ModelResource):
+    class Meta:
+        model = Status
+
+
+@admin.register(Status)
+class StatusAdmin(ImportExportModelAdmin):
+    resource_class = StatusResource
+
+
+class ConnetMethodResource(resources.ModelResource):
+    class Meta:
+        model = ConnetMethod
+
+
+@admin.register(ConnetMethod)
+class ConnetMethodAdmin(ImportExportModelAdmin):
+    resource_class = ConnetMethodResource
+
+
+class ParticipantResource(resources.ModelResource):
+    class Meta:
+        model = Participant
+
+
+@admin.register(Participant)
+class ParticipantAdmin(ImportExportModelAdmin):
+    resource_class = ParticipantResource
+
+
+class ServiceParticipantsResource(resources.ModelResource):
+    class Meta:
+        model = ServiceParticipants
+
+
+@admin.register(ServiceParticipants)
+class ServiceParticipantsAdmin(ImportExportModelAdmin):
+    resource_class = ServiceParticipantsResource
--- services/admin.py
+++ services/admin.py
@@ -3,3 +3,63 @@ from .models import DevicePublisher, DeviceUnit, Rack, Unit, DeviceStatus, Inter
 
 
 admin.site.register((Rack, Unit, DevicePublisher, DeviceUnit, DeviceStatus, InternetProvider, ProviderContract))
+
+
+class RackResource(resources.ModelResource):
+    class Meta:
+        model = Rack
+
+
+@admin.register(Rack)
+class RackAdmin(ImportExportModelAdmin):
+    resource_class = RackResource
+
+
+class UnitResource(resources.ModelResource):
+    class Meta:
+        model = Unit
+
+
+@admin.register(Unit)
+class UnitAdmin(ImportExportModelAdmin):
+    resource_class = UnitResource
+
+
+class DevicePublisherResource(resources.ModelResource):
+    class Meta:
+        model = DevicePublisher
+
+
+@admin.register(DevicePublisher)
+class DevicePublisherAdmin(ImportExportModelAdmin):
+    resource_class = DevicePublisherResource
+
+
+class DeviceUnitResource(resources.ModelResource):
+    class Meta:
+        model = DeviceUnit
+
+
+@admin.register(DeviceUnit)
+class DeviceUnitAdmin(ImportExportModelAdmin):
+    resource_class = DeviceUnitResource
+
+
+class DeviceStatusResource(resources.ModelResource):
+    class Meta:
+        model = DeviceStatus
+
+
+@admin.register(DeviceStatus)
+class DeviceStatusAdmin(ImportExportModelAdmin):
+    resource_class = DeviceStatusResource
+
+
+class InternetProviderResource(resources.ModelResource):
+    class Meta:
+        model = InternetProvider
+
+
+@admin.register(InternetProvider)
+class InternetProviderAdmin(ImportExportModelAdmin):
+    resource_class = InternetProviderResource
