187c16e3969ee2d0596184b1dffa4879a525d41f
user <user@linux.kali>
2023-05-02 10:14:44 +0500

    periodic task in testing

--- billing/tasks.py
+++ billing/tasks.py
@@ -9,6 +9,9 @@ from contracts.models import Contract
 
 from expertiseService.models import ExpertiseServiceContract
 
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 @shared_task
 def send_periodic_request():
@@ -23,7 +26,12 @@ def send_periodic_request():
                 colocation_contracts = Contract.objects.filter(Q(contract_status__name="To'lov kutilmoqda") | \
                                                                Q(contract_status__name="Aktiv"))
                 for contract in colocation_contracts:
+                    logger.info(f"contract >>>> {contract.id_code}")
+                    logger.info(f"contract_cash before >>>> {contract.contract_cash}")
+                    contract.payed_cash = 0
                     response = requests.post(url, data={'contract_id_code': contract.id_code})  # contract_id_code
+                    contract.save()
+                    logger.info(f"contract_cash after >>>> {contract.contract_cash}")
                     print(response.json())
             if 'EKSPERTIZA' in invoice_element.values():
                 expertise_contracts = ExpertiseServiceContract.objects.filter(Q(contract_status=3) | \
