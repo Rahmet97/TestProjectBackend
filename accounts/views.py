e0904bad8a4dd7226f8914ec5907920c7cdd0896
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-05-16 15:46:41 +0500

    PermissionListAPIView is permission_classes update to allowany

--- accounts/views.py
+++ accounts/views.py
@@ -121,9 +121,9 @@ class PermissionCreateAPIView(generics.CreateAPIView):
 
 
 class PermissionListAPIView(generics.ListAPIView):
-    # queryset = Permission.objects.all()
+    queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.IsAuthenticated,)
 
     # cache_backend = RedisCache
 
