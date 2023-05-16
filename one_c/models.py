8bf12c82d12a2c7e7d3d609b90482c058469fff4
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-05-16 11:46:30 +0500

    modified:   one_c/models.py, one_c/migrations/0003_alter_status_name.py

--- one_c/models.py
+++ one_c/models.py
@@ -7,7 +7,7 @@ from contracts.models import Contract, Service
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
     status_code = models.IntegerField(default=1)
 
     def __str__(self):
