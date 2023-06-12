942eb959cd817e79458dff4b3100a12d68801838
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-06-12 16:01:15 +0500

    vps app  added

--- TestProject/urls.py
+++ TestProject/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
 
     path('main/', include('main.urls')),
     path('expertise/', include("expertiseService.urls")),
-    path('vps/', include("vpsService.urls")),
+    # path('vps/', include("vpsService.urls")),
 
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
