04b462727e2abfa3dc5920e4eb383eaf9d08b768
user <user@linux.kali>
2023-06-19 15:04:07 +0500

    File Upload testing....

--- vpsService/views.py
+++ vpsService/views.py
@@ -12,6 +12,7 @@ from django.shortcuts import render
 from django.conf import settings
 from rest_framework import views, generics, permissions, response, status
 from django.core.files.storage import default_storage
+from rest_framework.decorators import api_view
 
 from accounts.models import UserData, YurUser, FizUser
 from contracts.models import AgreementStatus, Service, Participant
@@ -126,6 +127,7 @@ class NewFileCreateAPIView(views.APIView):
         return response.Response({'path': file_url, 'key': hashed_text})
 
 
+@api_view(['POST'])
 def convert_to_pdf(request):
     # Get the DOCX file from the request
     docx_file = request.FILES['docx_file']
