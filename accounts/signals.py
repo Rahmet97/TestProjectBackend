0f9fbe062cdf92e3bbdf55e6fdb8aa4ccc880e8e
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-06-21 16:48:47 +0500

    handle_user_role_change add

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 from rest_framework import status
 
@@ -10,6 +10,23 @@ from .models import UserData
 def handle_user_group_change(sender, instance, action, pk_set, **kwargs):
     if action == 'post_add':
         # The related groups are about to be cleared
+        existing_instance = UserData.objects.filter(
+            role=instance.role,
+            group__in=instance.group.all()
+        ).exclude(id=instance.pk)
+
+        if existing_instance.exists() and instance.status_action == instance.StatusChoices.EMPLOYEE:
+            # instance.group.remove(*existing_instance.last().group.all())
+            responseErrorMessage(
+                message="This combination of role and group already exists.",
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+
+
+@receiver(post_save, sender=UserData)
+def handle_user_role_change(sender, instance, created, **kwargs):
+    if not created:
+        # The related roles are about to be cleared
         existing_instance = UserData.objects.filter(
             role=instance.role,
             group__in=instance.group.all()
