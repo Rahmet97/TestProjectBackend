0c662cd48272e12791ace46ef4a874441544a21e
user <user@linux.kali>
2023-06-27 03:08:13 +0500

    Convert testing.

--- vpsService/views.py
+++ vpsService/views.py
@@ -139,7 +139,7 @@ class ConvertDocx2PDFAPIView(views.APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        url = 'http://localhost:81/ConvertService.ashx'
+        url = 'http://onlyoffice-documentserver/ConvertService.ashx'
         payload = {
             'async': False,
             'filetype': 'docx',
