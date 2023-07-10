61e64d376f5af3b4c538eedd059e14380ca5655d
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-07-10 16:51:01 +0500

    modified:   billing/serializers.py

--- billing/serializers.py
+++ billing/serializers.py
@@ -90,14 +90,15 @@ class InvoiceElementsSerializer(serializers.ModelSerializer):
 # VPS Serializer for billing automations
 class OperationSystemVersions(serializers.Serializer):
     operation_system_version = serializers.PrimaryKeyRelatedField(
-        queryset=OperationSystemVersion.objects.all(), pk_only=True
+        queryset=OperationSystemVersion.objects.all(), pk_field=serializers.UUIDField()
     )
     ipv_address = serializers.BooleanField(default=True)
 
 
 class VpsTariffSummSerializer(serializers.ModelSerializer):
     tariff = serializers.PrimaryKeyRelatedField(
-        required=False, allow_null=True, queryset=VpsTariff.objects.all(), pk_only=True
+        required=False, allow_null=True, queryset=VpsTariff.objects.all(),
+        pk_field=serializers.IntegerField()
     )
     operation_system_versions = OperationSystemVersions(many=True)
 
